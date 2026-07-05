2d2314b56026a7a87a665ea850731a303ca69cbb
Ranger <mnipk1243@gmail.com>
2026-07-06 00:15:34 +0500

    commented out changes

--- app/src/main/java/com/example/android_doc_app/handlers/PdfHandler.kt
+++ app/src/main/java/com/example/android_doc_app/handlers/PdfHandler.kt
@@ -3,6 +3,6 @@ package com.example.android_doc_app.handlers
 import android.content.Intent
 import android.net.Uri
 
-fun pdfHandler(intent: Intent){
-    val pdfUrl = intent.getParcelableExtra<Uri>(intent.EXTRA_STREAM)
-}
+//fun pdfHandler(intent: Intent){
+//    val pdfUrl = intent.getParcelableExtra<Uri>(intent.EXTRA_STREAM)
+//}
